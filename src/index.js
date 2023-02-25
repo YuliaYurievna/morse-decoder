863bbee6fd723fadee3d07ddb8d1907034a642ed
Yuliia Poddiacha <104911480+YuliaYurievna@users.noreply.github.com>
2023-02-25 18:14:06 +0200

    feat: add a solution of the task

--- src/index.js
+++ src/index.js
@@ -1,46 +1,67 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  ".-": "a",
+  "-...": "b",
+  "-.-.": "c",
+  "-..": "d",
+  ".": "e",
+  "..-.": "f",
+  "--.": "g",
+  "....": "h",
+  "..": "i",
+  ".---": "j",
+  "-.-": "k",
+  ".-..": "l",
+  "--": "m",
+  "-.": "n",
+  "---": "o",
+  ".--.": "p",
+  "--.-": "q",
+  ".-.": "r",
+  "...": "s",
+  "-": "t",
+  "..-": "u",
+  "...-": "v",
+  ".--": "w",
+  "-..-": "x",
+  "-.--": "y",
+  "--..": "z",
+  ".----": "1",
+  "..---": "2",
+  "...--": "3",
+  "....-": "4",
+  ".....": "5",
+  "-....": "6",
+  "--...": "7",
+  "---..": "8",
+  "----.": "9",
+  "-----": "0",
 };
 
 function decode(expr) {
-    // write your solution here
+  const charactersArray = expr.match(/.{10}/g);
+  const words = charactersArray
+    .map((character) => {
+      const deleteEmptyCharacters = parseInt(character).toString();
+      const splitCharacters = deleteEmptyCharacters.match(/.{2}/g);
+      const characterMorseCode = splitCharacters
+        .map((el) => {
+          switch (el) {
+            case "10":
+              return `.`;
+            case "11":
+              return `-`;
+            case "Na":
+              return ` `;
+          }
+        })
+        .join("");
+      const convertCharacter = MORSE_TABLE[characterMorseCode] || " ";
+      return convertCharacter;
+    })
+    .join("");
+  return words;
 }
 
 module.exports = {
-    decode
-}
+  decode,
+};
